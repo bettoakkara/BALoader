34763ce16ae7b732c83d3db591f726e0e181363a
Betto Akkara <bettoakkara1@gmail.com>
2021-02-02 23:04:58 +0530

    initial commit

---
+++ BALoader/BALoader.h
@@ -0,0 +1,18 @@
+//
+//  BALoader.h
+//  BALoader
+//
+//  Created by Betto Akkara on 02/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BALoader.
+FOUNDATION_EXPORT double BALoaderVersionNumber;
+
+//! Project version string for BALoader.
+FOUNDATION_EXPORT const unsigned char BALoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BALoader/PublicHeader.h>
+
+
